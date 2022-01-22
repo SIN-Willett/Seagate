bb58f6af01819b5193022db641097daa4955955f
Omen-Red <98036297+Omen-Red@users.noreply.github.com>
2022-01-23 08:38:41 +1000

    auto gold

--- Downtimes/Main/downtime.py
+++ Downtimes/Main/downtime.py
@@ -2,7 +2,7 @@
 <drac2>
 if not get("stats"):
     err("Stats not set")
-args = &ARGS&
+args = &&&
 downtime_name = str(args[0])
 if not args[1].isnumeric():
     err(args[1] + " is not a valid DC for " + args [0])
@@ -40,7 +40,7 @@ for check in our_downtime:
         elif proficiency:
             potential_mod += (proficiencyBonus)
         
-        if potential_mod > best_mod:
+        if potential_mod > best_mod or best_mod == 0:
             best_mod = potential_mod
             best_option = option
             
@@ -71,23 +71,86 @@ if sum(roll.result.crit == 1 for roll in rolls) > 0:
     successes = num_checks
 
 # respond
-responses = load_json(get_gvar("2764f057-457a-483d-bb70-591863c8f695"))["illegal" if (num_checks == 3) else "legal"]
-if num_checks == 3 and  successes == 0:
-    response = f"You are now wanted for a DC {vroll(f'{dc}+5').total} crime! Ping a moderator, and let an admin know to add you to the wanted board."
+crime = (num_checks == 3)
+strdc = str(dc)
+data = load_json(get_gvar("cd374b66-bf6e-4ef9-ac64-d9e213a5c0cb"))["illegal" if crime else "legal"]
+
+xp = data[strdc]["XP"]
+gp = data[strdc]["Gold"]
+
+total_earned = gp
+response = ""
+coin_response = f"You now have "
+if crime:
+    if not successes:
+        response = f"You are now wanted for a DC {vroll(f'{dc}+5').total} crime! Ping a moderator, and let an admin know to add you to the wanted board."
+        total_earned = 0
+        xp = 0
+    if successes == 1:
+        response = f"You are now wanted for your crimes! Ping a moderator, and let an admin know to add you to the wanted board."
+    if successes == 2:
+        total_earned = (gp * 0.5)
+        xp = int(xp * 0.5)
+        response = f"You earn {xp} * {level} = {xp * level} XP and "
+    else:
+        response = f"You earn {xp} * {level} = {xp * level} XP and "
 else:
-    response = responses[successes]
+    if not successes:
+        response = f"You earn no GP and no experience."
+        total_earned = 0
+        xp = 0
+    if successes == 1:
+        total_earned = (gp * 0.5)
+        xp = int(xp * 0.5)
+        response = f"You earn {xp} * {level} = {xp * level} XP and "
+    else:
+        response = f"You earn {xp} * {level} = {xp * level} XP and "
+
+gp_gained = floor(total_earned)
+sp_gained = floor((total_earned - gp_gained) * 10)
+cp_gained = int((((total_earned - gp_gained) * 10) - sp_gained) * 10)
+
+bags = load_json(get("bags"))
+i = 0
+for bag in bags:
+    if bag[0] == "Coin Pouch":
+        break
+    i = i + 1
+
+if i == len(bags):
+    err("you have no coin pouch")
+
+bags[i][1]['cp'] = int(bags[i][1]['cp'] + cp_gained)
+bags[i][1]['sp'] = int(bags[i][1]['sp'] + sp_gained)
+bags[i][1]['gp'] = int(bags[i][1]['gp'] + gp_gained)
+
+total_gp = bags[i][1].gp
+total_sp = bags[i][1].sp
+total_cp = bags[i][1].cp
+character().set_cvar("bags", dump_json(bags))
+
+if cp_gained != 0:
+    response += f"{sp_gained} SP and {cp_gained} CP!"
+    coin_response += f"{total_sp} SP and {total_cp} CP!"
+elif sp_gained != 0:
+    response += f"{gp_gained} GP and {sp_gained} SP!"
+    coin_response += f"{total_gp} GP and {total_sp} SP!"
+else:
+    response += f"{gp_gained} GP!"
+    coin_response += f"{total_gp} GP!"
 
 output_text = '\n'
 for i in range(num_checks):
     success = 'Success!' if rolls[i].total >= dc else 'Failure!'
-    output_text += '**' + checks[i] + ': **' + rolls[i] + '** ' + success + '**\n'
+    output_text += '**' + checks[i] + ':** ' + rolls[i] + ' **' + success + '**\n'
 if "guid" in args:
-    output_text += '**' + "Guidance" + ': **' + vroll(f'1d4') + ' add this to one of the above rolls!' + '\n'
+    output_text += "**Guidance: **" + vroll(f"1d4") + " add this to one of the above rolls!\n"
 </drac2>
 
 -title "{{name}} does the {{downtime_name}} downtime!"
 -desc "**DC:** {dc} {{output_text}}"
 -footer "{{response}}
+{{coin_response}}
 Made by Omen & Sin"
 -color <color>
 -thumb <image>
