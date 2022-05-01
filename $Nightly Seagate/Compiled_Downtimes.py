057f699d0042336fdf5edbe8a96d10be84f86f48
Omen Red <98036297+Omen-Red@users.noreply.github.com>
2022-05-02 00:19:38 +1000

    Beginning refactor for functions using nightly

---
+++ $Nightly Seagate/Compiled_Downtimes.py
@@ -0,0 +1,82 @@
+
+tembed
+{{get_gvar("65fcab20-e83d-4cdd-9260-84e4ee62d052")}}
+# 2c0daf9b-fc9e-47ee-bd7b-9850ac961d6f
+
+{{create_cc_nx("Experience")}}
+{{calendar=load_json(get_gvar("fc3cc24c-e1c9-42e2-a6d2-ab39ce36e19c"))}}
+# 1aec09a0-9e25-4700-9c2d-42d79cb0163b
+{{hourOffset=calendar.get('hourOffset',0)+int(get('timezone',0))}}
+{{baseYear=calendar.get("yearOffset",1970)}}
+{{Time=time()+(3600*hourOffset)}}
+{{totalDayCount=int((Time)//86400)}}
+{{yearsPassed=totalDayCount//calendar.length}}
+{{numLeapYears=len([x for x in range(baseYear,baseYear+yearsPassed-4) if x//calendar.get('leapCycle',4)==x/calendar.get('leapCycle',4)]) if calendar.get('leapCycle') else 0}}
+{{yearStartDay=yearsPassed*calendar.length+numLeapYears}}
+{{totalDays=totalDayCount-yearStartDay}}
+{{year=int((totalDayCount-numLeapYears-1)//calendar.length)+baseYear}}
+{{isLeapYear=year//calendar.get('leapCycle',4)==year/calendar.get('leapCycle',4) if numLeapYears else 0}}
+{{calendarDay=totalDays%(calendar.length+isLeapYear) or calendar.length+isLeapYear}}
+{{hour=int(Time%86400//3600)}}
+{{minute=int(Time%86400%3600//60)}}
+{{second=int(Time%86400%3600%60)}}
+{{monthLengths=[x.length+(isLeapYear and x.name==calendar.get('leapMonth','February')) for x in calendar.months]}}
+{{day,month=calendarDay,1}}
+{{[(day:=day-monthLengths[x],month:=month+1) for x in range(len(monthLengths)) if month>x and day>monthLengths[x]]}}
+{{timestamp=f'{day:02}.{month:02}.{str(year)[2:]} ({hour:02}:{minute:02}:{second:02})'}}
+{{character().set_cvar("Timestamp",timestamp)}} 
+<drac2>
+## The above timestamp generation is sourced from Derixyleth#0636 xplog alias
+args = &ARGS&
+## This is to sub in a saved DT
+if args[0].isnumeric():
+    index = args[0]
+    DTsaved = get("DTsaved")
+    
+    if not DTsaved:
+        err(f"Use !dt save or !downtime save {index} the downtime deatils you want to save to this number.")
+    
+    possible = load_json(DTsaved)
+    
+    if index not in possible:
+        err(f"There is no downtime saved to {index}")
+    
+    args = possible[index]
+
+    if len(args) >= 1:
+        args += &ARGS&[1:]
+    
+strargs = str(args)
+DT = args[0]
+
+if DT is "test":
+        err("This is the live downtime alias, are you trying to use !testdt ?")
+
+def check_dt(dt_name):
+    if DT in dt_name:
+        return True
+
+def do_dt(dt_address):
+    return get_gvar(dt_address).replace("&&&", strargs)
+
+if check_dt(["save"]):
+    return do_dt("83faff8e-8cdb-4758-9c36-a7882279dfea")
+
+if check_dt(["service"]):
+    return do_dt("e55f5a53-b70a-484d-a386-6acc350d2c0d")
+
+if check_dt(["work", "working", "job"]):
+    return do_dt("99ae5250-5616-4847-8f27-3237cc4eb0b7")
+# 1231c3de-651d-4d19-abfe-185c09b785c7
+
+if check_dt(["craft", "crafting"]):
+    return do_dt("4bd5ceb6-e82f-4196-be25-988c6c4e2a3d")
+
+if check_dt(["train", "training"]):
+    return do_dt("585d1453-7303-44a5-b736-f707c2702b5e")
+
+if check_dt(["study", "studying"]):
+    return do_dt("97d2dddb-2116-41e3-a312-44351619d3eb")
+
+return do_dt("da0f7936-97bd-472c-b3fd-f72622df20e4")
+</drac2>
---
+++ $Nightly Seagate/crafting.py
@@ -0,0 +1,186 @@
+<drac2>
+# gvar 4bd5ceb6-e82f-4196-be25-988c6c4e2a3d
+args = &&&
+pars = argparse(args).last
+##gvar for items 1st dict "Magic", 2nd dict "Mundane"
+craftables = load_json(get_gvar("c28ac8be-d70e-4842-8f5c-b9e251ce4eb6"))
+inputcraft = args[1]
+crafting_details = None
+item_name = None
+crafting_type = None
+
+for craftableCategory, stuff in craftables.items():
+    if crafting_details:
+        break
+        
+    for craftableName, craftableDetails in stuff.items():
+        if (inputcraft.lower() == craftableName.lower()):
+            item_name = craftableName
+            crafting_details = craftableDetails
+            crafting_type = craftableCategory
+        elif (inputcraft.lower() in craftableName.lower()):
+            item_name = craftableName
+            crafting_details = craftableDetails
+            crafting_type = craftableCategory
+            break
+    
+if not crafting_details:
+# name not in list
+    err("You have failed to select a craftable, please check the name of the item you want to craft against the crafting spreadsheet <3")
+
+magic_data = {
+	"Common"	:	{"actions": 7, "price": 100, "ss": 1, "ss_art": 1},
+	"Uncommon"	:	{"actions": 15, "price": 200, "ss": 3, "ss_art": 2},
+	"Rare"		:	{"actions": 40, "price": 1000, "ss": 5, "ss_art": 3},
+	"Very Rare"	:	{"actions": 120, "price": 2000, "ss": 6, "ss_art": 4},
+	"Legendary"	:	{"actions": 300, "price": 20000, "ss": 9, "ss_art": 5}
+}
+
+rarity = None
+price = None
+tool = None
+actions = None
+ss      = None
+ss_art  = None
+
+if crafting_type == "Magic":
+    rarity  = crafting_details.rarity
+    tool    = crafting_details.tool
+    actions = magic_data[rarity].actions
+    price    = magic_data[rarity].price
+    ss      = magic_data[rarity].ss
+    ss_art  = magic_data[rarity].ss_art
+else:
+    price   = crafting_details.price
+    tool    = crafting_details.tool
+    actions = ceil(price / 10)
+
+# makes the minimum 1 action
+if actions < 1:
+    actions = 1
+# makes the actions 3 for house related things
+if not price:
+    actions = 3
+
+cc = "Crafting: " + item_name
+
+##makes counter if it doesn't already exist
+character().create_cc_nx(cc, 0, actions, None, None, 0)
+if (get_cc(cc) == actions) or (price == 0):
+    character().set_cc(cc, 0)
+    
+##cvar for tool to attribute matching
+stats = load_json(get("stats"))
+mod = stats[tool].mod
+arcana = character().skills.arcana.bonus + (character().skills.arcana.prof * proficiencyBonus)
+magic_response = None
+        
+artificer_level = 0
+for (cls, class_level) in character().levels:
+    if cls == "Artificer":
+        if class_level > 1:
+            artificer_level = class_level
+            ss = ss_art
+    break
+
+if crafting_type == "Magic":
+    mod += int(arcana)
+    if character().spellbook.get_slots(ss) < 1:
+        err("You do not have enough spell slots remaining")
+    else:
+        character().spellbook.use_slot(ss)
+        magic_response = f"You use a spell slot for crafting: {character().spellbook.slots_str(ss)}"
+if crafting_type == "Mundane":
+    if ("exp" in args) or (tool.lower() in get('eTools','').lower()):
+        mod += ((proficiencyBonus) * 2)
+    elif ("prof" in args) or (tool.lower() in get('pTools','').lower()):
+        mod += (proficiencyBonus)
+
+mydice = ""
+if "adv" in args:
+    mydice += f"2d20kh1+{mod}"
+elif "dis" in args:
+    mydice += f"2d20kl1+{mod}"
+else:
+    mydice += f"1d20+{mod}"
+if "guid" in args:
+    mydice += "+1d4[guidance]"
+if "ls" in args and crafting_type == "Mundane":
+    mydice += "+1[luckstone]"
+if "-b" in args or "-bonus" in args:
+    bonus = pars("b", pars("bonus",""))
+    mydice += ("+" + bonus + "[bonus]")
+
+myroll = vroll(mydice)
+
+cost = (price * 0.5)
+sell = round((price * 0.75), 2)
+magic_item_adept = False
+if (rarity == "Common") or (rarity == "Uncommon"):
+    magic_item_adept = True
+registered = get("Guild")
+if "guild" in args or registered:
+    cost = (cost * 0.75)
+
+xp = 0
+
+def reward(dice_0, dice_1, dice_2, xp_value, cc_1, cc_2, cc_3, cost_mod):
+    if myroll.total < dice_0:
+        xp = 0
+    elif myroll.total < dice_1:
+        character().mod_cc(cc, +cc_1)
+        xp = xp_value
+        cost = (cost * cost_mod)
+    elif myroll.total < dice_2:
+        character().mod_cc(cc, +cc_2)
+        xp = xp_value
+        cost = (cost * cost_mod)
+    else:
+        character().mod_cc(cc, +cc_3)
+        xp = xp_value
+        cost = (cost * cost_mod)
+
+if crafting_type is "Mundane":
+    reward(10, 16, 21, 15, 1, 2, 3, 1)
+elif (artificer_level >= 10) and (magic_item_adept is True):
+    reward(15, 21, 26, 30, 4, 8, 12, 0.5) 
+else:
+    reward(15, 21, 26, 30, 1, 2, 3, 1) 
+
+total_xp = xp * level
+
+current_actions = character().get_cc(cc)
+xp_args = "" + total_xp + " |  " + cc + " (" + current_actions + "/" + actions + ")"
+exp_cc = "Experience"
+xplog = load_json(get('xplog','{}'))
+timestamp = get("Timestamp")
+xplog.update({timestamp:xp_args})
+set_cvar('xplog',dump_json(xplog))
+mod_cc(exp_cc,total_xp)
+
+char_xp = get_cc(exp_cc)
+
+xplog_response = f"You now have {char_xp} XP and your most recent xplog entry will be:"
+
+title = f'{name} does the crafting downtime for {item_name}'
+response_xp = ()
+if xp == 0:
+    response_xp = f"You failed crafting and gain no xp"
+else:
+    response_xp = f"Gain {xp} * {level} = {total_xp} XP!"
+</drac2>
+-title "{{title}}"
+-desc "**{{tool}}:** {{myroll}}
+
+Your progress on crafting this is now __{{current_actions}}/{{actions}}__!
+{{magic_response}}"
+-footer "{{response_xp}}
+
+This item costs {{round(cost, 2)}} GP to make and sells to players for {{price}} GP and to the void for {{sell}} GP!
+
+{{xplog_response}}
+{{timestamp}}: {{xp_args}}
+
+Made by Omie <3"
+-color <color>
+-thumb <image>
