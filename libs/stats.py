564842843f1827bb24627ff77e6f0e854ff5e5cb
Sioryn Indianna Nereyeus  Willett <s4392620@student.uq.edu.au>
2022-07-14 05:03:26 +1000

    stats up to 4.1

--- libs/stats.py
+++ libs/stats.py
@@ -1,11 +1,11 @@
 !gvar edit f5318e22-c75c-460f-96b4-7985b1c2bbd7
-from avrae import *
-from yaml import *
 
-<drac2>
-ETOOLS = get_yaml('eTools', [])
-PTOOLS = get_yaml('pTools', [])
-PLANGS = get_yaml('languages', [])
+using(libs="02488881-7259-4a76-9362-828df7c35a07")
+using(yaml=libs.find_library("yaml"))
+
+ETOOLS = yaml.load('eTools', [])
+PTOOLS = yaml.load('pTools', [])
+PLANGS = yaml.load('languages', [])
 PRONOUNS = [p.lower().strip() for p in get('pronouns', 'they/them/their').split('/', 2)]
 
 def _init_stat(mod, message=None, is_tool=False):
@@ -13,52 +13,27 @@ def _init_stat(mod, message=None, is_tool=False):
 
 def _init_tool(name, mod, message=None):
   prof_level = 2 if name in ETOOLS else 1 if name in PTOOLS else 0
-  mod += proficiencyBonus * prof_level
+  mod += character().stats.prof_bonus * prof_level
   return _init_stat(mod, message, True)
 
 def _init_lang(name, message=None):
-  mod = intelligenceMod
+  mod = character().stats.get_mod("INT")
   if name in PLANGS:
-    mod += proficiencyBonus
+    mod += character().stats.prof_bonus
   return _init_stat(mod, message, None)
 
 def _init_arm():
   return _init_stat(character().skills.athletics.value, None, None)
 
 def _init_instr(name):
-  return _init_tool(name, charismaMod, f'is playing the {name.lower()}')
+  return _init_tool(name, character().stats.get_mod("CHA"), f'is playing the {name.lower()}')
 
-_instr_data = get_yaml('g/186f07ad-aeec-435a-a450-c5c47bfc5f39')
+_instr_data = yaml.load('g/186f07ad-aeec-435a-a450-c5c47bfc5f39')
 INSTRUMENTS = {name: _init_instr(name) for name in _instr_data}
 
-TOOLS = INSTRUMENTS | {name: _init_tool(name, mod, msg) for name, mod, msg in [
-  ("Dice Set", charismaMod, "is throwing dice"),
-  ("Playing Card Set", charismaMod, "is playing cards"),
-  ("Cobbler's Tools", dexterityMod, "is cobbling"),
-  ("Disguise Kit", dexterityMod, f"has disguised {PRONOUNS[1]}self"),
-  ("Glassblower's Tools", dexterityMod, "is glassblowing"),
-  ("Jeweler's Tools", dexterityMod, "is working on jewelery"),
-  ("Potter's Tools", dexterityMod, "is working on pottery"),
-  ("Tinker's Tools", dexterityMod, "is tinkering"),
-  ("Thieves' Tools", dexterityMod, "is using Thieves' tools"),
-  ("Weaver's Tools", dexterityMod, "is weaving"),
-  ("Woodcarver's Tools", dexterityMod, "is carving"),
-  ("Alchemist's Supplies", intelligenceMod, "is doing alchemy"),
-  ("Calligrapher's Supplies", intelligenceMod, "is doing calligraphy"),
-  ("Cartographer's Tools", intelligenceMod, "is doing cartography"),
-  ("Poisoner's Kit", intelligenceMod, "is working with poisons"),
-  ("Carpenter's Tools", strengthMod, "is doing carpentry"),
-  ("Leatherworker's Tools", strengthMod, "is doing leatherwork"),
-  ("Mason's Tools", strengthMod, "is doing masonary"),
-  ("Smith's Tools", strengthMod, "is smithing"),
-  ("Brewer's Supplies", wisdomMod, "is brewing"),
-  ("Cook's Utensils", wisdomMod, "is cooking"),
-  ("Herbalism Kit", wisdomMod, "is working with herbs"),
-  ("Painter's Supplies", wisdomMod, "is painting"),
-  ("Navigator's Tools", wisdomMod, "is navigating"),
-  ("Land Vehicles", strengthMod, None),
-  ("Water Vehicles", strengthMod, None),
-]}
+_tool_specs = yaml.load("g/041c13c8-9fbc-4fdc-b980-646568f87368")
+
+TOOLS = INSTRUMENTS | {name: _init_tool(name, character().stats.get_mod(mod), msg) for name, mod, msg in _tool_specs}
 
 SKILLS = { name: _init_stat(mod, msg) for name, mod, msg in [
   ("Performance", character().skills.performance.value, "is performing"),
@@ -81,10 +56,10 @@ SKILLS = { name: _init_stat(mod, msg) for name, mod, msg in [
   ("Intimidation", character().skills.intimidation.value, "is being intimidating"),
 ]}
 
-_lang_data = get_yaml('g/3ea94488-2b7f-45e8-a7e3-7b3c8b6a5ec7')
+_lang_data = yaml.load('g/3ea94488-2b7f-45e8-a7e3-7b3c8b6a5ec7')
 LANGS = { name: _init_lang(name) for name in _lang_data}
 
-_arms_data = get_yaml('g/f9f9b842-9924-48e9-98a3-bc12e739b3df')
+_arms_data = yaml.load('g/f9f9b842-9924-48e9-98a3-bc12e739b3df')
 ARMS = { name: _init_arm() for name in _arms_data}
 
 STATS = TOOLS | SKILLS | LANGS | ARMS
@@ -97,6 +72,4 @@ def best_stat(stat_choices):
       best = choice
   return best
 
-
-character().set_cvar("stats", dump_yaml(stats, 1))
-</drac2>
+yaml.dump("stats", stats)
